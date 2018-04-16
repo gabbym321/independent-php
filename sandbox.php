01ee90f5c463aab65d0073425faa42811468c4f2
GABRIEL MAKOWER <gabrielm1571@hstat.org>
2018-04-16 15:59:04 +0000

    try push

---
+++ sandbox.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>JS Bin</title>
+</head>
+<body>
+<?php
+  print('Hello, World!');
+
+  print("\nThe sum of 2 and 3 is 5.");
+
+  $sum = (int)$argv[1] + (int)$argv[2];
+
+  print("\nThe sum of $argv[1] and $argv[2] is $sum.");
+?>
+  
+</body>
+</html>
